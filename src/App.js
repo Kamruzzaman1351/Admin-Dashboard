630bd92d4224aecdc7f2bb706134ebea53b373d9
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-02 21:00:47 +0600

    Installing the necessary packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import "./App.css"
 function App() {
   return (
     <div>
-        <h2>Admin Dashboard</h2>
+      <h2 className="text-white text-5xl bg-blue-500 text-center">Admin Dashboard</h2>
     </div>
   )
 }

7c3cfad2035109e01d4611d2a34981aba5cc2d32
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-02 22:09:10 +0600

    Creating the dummy data, setup the folders and work on the app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import "./App.css"
+import React, { useEffect } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { FiSettings } from "react-icons/fi"
+import { TooltipComponent } from "@syncfusion/ej2-react-popups"
 function App() {
+  const isActiveMenu = true;
   return (
-    <div>
-      <h2 className="text-white text-5xl bg-blue-500 text-center">Admin Dashboard</h2>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{zIndex: "1000"}}>
+            <TooltipComponent content="Settings" position="Top">
+              <button type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{background: "blue", borderRadius: "50%"}}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {isActiveMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              {/* SideBar Goes Here */}
+              SideBar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              {/* SideBar Goes Here */}
+              SideBar
+            </div>
+          )}
+          <div className= { 
+              `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full
+              ${isActiveMenu ? "md:ml-72 " : "flex-2"}`}>
+              <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+                {/* NavBar GOes Here */}
+                NavBar
+              </div>
+          </div>
+          <div>
+            <Routes>
+              {/* Route Goes Here */}
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+      
+    </>
   )
 }
 

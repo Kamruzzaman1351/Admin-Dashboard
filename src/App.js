f1cfdd371ea94ee585b8438dd2ad832554ab7252
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-02 23:52:18 +0600

    Creating all the pages and components and making the routes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
 import "./App.css"
 import React, { useEffect } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { FiSettings } from "react-icons/fi"
-import { TooltipComponent } from "@syncfusion/ej2-react-popups"
+import { SettingsIconButton, Sidebar, Footer, Navbar } from "./components"
+import { Dashboard, Calendar, ColorPicker, Customers, Editor, Employees, Kanban, Orders, Area, Bar, ColorMapping, Financial, Line, Pie, Pyramid, Stacked } from "./pages"
 function App() {
   const isActiveMenu = true;
   return (
     <>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{zIndex: "1000"}}>
-            <TooltipComponent content="Settings" position="Top">
-              <button type="button"
-                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{background: "blue", borderRadius: "50%"}}>
-                <FiSettings />
-              </button>
-            </TooltipComponent>
-          </div>
+          <SettingsIconButton />
           {isActiveMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
               {/* SideBar Goes Here */}
-              SideBar
+              <Sidebar />
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
               {/* SideBar Goes Here */}
-              SideBar
+              <Sidebar />
             </div>
           )}
           <div className= { 
-              `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full
-              ${isActiveMenu ? "md:ml-72 " : "flex-2"}`}>
-              <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-                {/* NavBar GOes Here */}
-                NavBar
-              </div>
-          </div>
-          <div>
-            <Routes>
-              {/* Route Goes Here */}
-            </Routes>
+            `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full
+            ${isActiveMenu ? "md:ml-72 " : "flex-2"}`}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              {/* NavBar GOes Here */}
+              <Navbar />
+            </div>
+            <div>
+              <Routes>
+                {/* Dashboard */}
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                {/* Pages  */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+
+                {/* APPs  */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
+
+                {/* Charts  */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
+              </Routes>
+            </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>

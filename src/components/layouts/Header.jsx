fcb86115d4a875973325960df372e53cf1efa3dc
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-04 21:29:51 +0600

    Creating Header components and dev the Order page with Syncfusion Grid components

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
-const Header = () => {
+
+const Header = ({ title, catagory }) => {
   return (
-    <div>Header</div>
+    <div className="mb-10">
+      <p className="text-gray-400">{catagory}</p>
+      <p className="text-3xl font-extrabold tracking-tight">{title}</p>
+    </div>
   )
 }
 
+Header.defaultProps = {
+  title: "Page Title",
+  catagory: "Catagory"
+}
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  catagory: PropTypes.string.isRequired,
+}
 export default Header
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Header } from '../components'
+import {GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject} from "@syncfusion/ej2-react-grids"
+import {ordersData, contextMenuItems, ordersGrid} from "../data/dummyData"
+
 
 const Orders = () => {
+  const editing = { allowDeleting: true, allowEditing: true };
   return (
-    <div>Orders</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Order" catagory="Page"/>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging={true}
+        allowSorting={true}
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        editSettings={editing}
+      >
+        <ColumnsDirective>
+          { ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          )) }
+        </ColumnsDirective>
+        <Inject 
+          services={[Resize, Sort, Filter, Page, ContextMenu, ExcelExport, PdfExport, Edit]}  
+        />
+      </GridComponent>
+    </div>
   )
 }
 

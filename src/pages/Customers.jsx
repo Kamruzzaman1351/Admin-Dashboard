33e2f31f06ae8b75457e1b87706de61112f21249
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-04 23:54:08 +0600

    Creating Customers page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Header } from '../components';
+import { GridComponent, ColumnsDirective, ColumnDirective, Search, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, Toolbar } from "@syncfusion/ej2-react-grids"
+import { customersData, customersGrid, contextMenuItems } from "../data/dummyData"
+
 
 const Customers = () => {
+  const toolbarOptions = ['Search', 'Delete'];
+
+  const editing = { allowDeleting: true, allowEditing: true };
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <Header title="Customers" catagory="Page" />
+        <GridComponent
+          dataSource={customersData}
+          width="auto"
+          allowPaging
+          allowSorting
+          contextMenuItems={contextMenuItems}
+          pageSettings={{ pageCount: 5 }}
+          editSettings={editing}
+          toolbar={toolbarOptions}
+          allowExcelExport
+          allowPdfExport
+        >
+          <ColumnsDirective>
+            {customersGrid.map((item, index) => (
+              <ColumnDirective key={index} {...item} />
+            ))}
+          </ColumnsDirective>
+          <Inject services={[Search, Sort, Filter, Page, Toolbar, ExcelExport, PdfExport, Edit,ContextMenu]} />
+        </GridComponent>
+    </div>
   )
 }
 

b67d5bd71afe5c2ae9711a4f16a000ef67e0e044
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-06 21:45:45 +0600

    Creating Notification components

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -12,7 +12,11 @@ const Chat = () => {
       <div className="flex justify-between items-center">
         <div className="flex gap-3">
           <p className="font-semibold text-lg dark:text-gray-200">Messages</p>
-          <button type="button" className="text-white  text-xs rounded p-1 px-2 bg-orange">
+          <button type="button" className="text-white  text-xs rounded p-1 px-2"
+            style={{
+              backgroundColor: currentColor
+            }}
+          >
             5 New
           </button>
         </div>

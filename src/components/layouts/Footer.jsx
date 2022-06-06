88677160099a5d39e23fe2527927b89d93e1ebbf
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-06 22:40:15 +0600

    Creating UserProfile and Footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import DashboardContext from '../../contexts/DashboardContext'
+
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
+  const { currentColor } = useContext(DashboardContext)
   return (
-    <div>Footer</div>
+    <div className="mt-24 border-t-1 border-color"
+      style={{
+        backgroundColor: currentColor
+      }}
+    >
+      <p className="text-white text-center py-10">
+        © {currentYear} All rights reserved.
+      </p>
+    </div>
   )
 }
 

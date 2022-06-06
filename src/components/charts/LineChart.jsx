3793eecfcaf5d9fc8122832cbb2abdbd37ec1b4a
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-06 19:35:45 +0600

    Creating the line chart

--- src/components/charts/LineChart.jsx
+++ src/components/charts/LineChart.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective,
+        Inject, DateTime, Legend, Tooltip, LineSeries
+      } from "@syncfusion/ej2-react-charts"
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis, lineChartData } from '../../data/dummyData'
+
+
 
 const LineChart = () => {
   return (
-    <div>LineChart</div>
+    <ChartComponent
+      id="line-chart"
+      height="450px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      tooltip={{enable: true}}
+      chartArea={{border:{width:0}}}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip,]}/>
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
--- src/pages/charts/Line.jsx
+++ src/pages/charts/Line.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Header, LineChart } from '../../components'
+
+
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Line Chart" catagory="Chart" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
   )
 }
 
